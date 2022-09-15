54ff32352a48c3886b6452e1a6ed8d45cfd46fe3
Kyohei Uto <kyoheiu@outlook.com>
2022-09-15 12:01:40 +0900

    Create bit array, needs test

--- main.zig
+++ main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const TIME_BITS = 48;
+const ULID_BIT_LEN = 128;
 const rndGen = std.rand.DefaultPrng;
 const MAX_RAND: u128 = (1 << 80) - 1;
 
@@ -20,22 +21,25 @@ const Ulid = struct {
         };
     }
 
-    pub fn ulid(self: *Self) UlidError!void {
+    pub fn ulid(self: *Self) UlidError![]u1 {
         const timestamp = @intCast(u64, std.time.milliTimestamp());
         if (self.time != undefined) {
             if (self.time == timestamp) {
                 if (self.rand != MAX_RAND) {
                     self.rand += 1;
+                    return self.gen_ulid_bit();
                 } else {
                     return UlidError.MaxRand;
                 }
             } else {
                 self.time = @as(u64, timestamp);
                 self.gen_rand();
+                return self.gen_ulid_bit();
             }
         } else {
             self.time = timestamp;
             self.gen_rand();
+            return self.gen_ulid_bit();
         }
     }
 
@@ -45,12 +49,43 @@ const Ulid = struct {
         const rand16 = rnd.random().int(u16);
         const rand64 = rnd.random().int(u64);
         self.rand = @as(u128, rand16) << 64 | rand64;
-        std.debug.print("{b}\n", .{self.rand});
+    }
+
+    fn gen_ulid_bit(self: Self) []u1 {
+        const ulid_int: u128 = @as(u128, self.time) << 80 | self.rand;
+        std.debug.print("{}\n", .{ulid_int});
+        std.debug.print("{b}\n", .{ulid_int});
+        return to_binary(ulid_int);
     }
 };
 
+fn to_binary(value: u128) []u1 {
+    var v = value;
+    var result: [ULID_BIT_LEN]u1 = undefined;
+    var j: u8 = 0;
+    var bit: u1 = 0;
+    while (j < ULID_BIT_LEN) {
+        std.debug.print("{}", .{result[j]});
+        j += 1;
+    }
+    std.debug.print("\n", .{});
+    var i: u8 = 0;
+    while (i < ULID_BIT_LEN) {
+        bit = @intCast(u1, v & 1); 
+        std.debug.print("{}", .{bit});
+        result[ULID_BIT_LEN - i - 1] = bit;
+        v = v >> 1;
+        i += 1;
+   }
+        std.debug.print("\n", .{});
+   return &result;
+}
 
 pub fn main() !void {
     var gen = Ulid.new();
-    try gen.ulid();
+    const ulid = try gen.ulid();
+    std.debug.print("{}\n", .{ulid.len});
+    for (ulid) |x| {
+        std.debug.print("{}", .{x});
+    }
 }
