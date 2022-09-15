75593f86c4d7b523f33a1ec56cd7e29272b9b595
Kyohei Uto <kyoheiu@outlook.com>
2022-09-15 22:45:44 +0900

    Fix test

--- ulid.zig
+++ ulid.zig
@@ -7,11 +7,11 @@ const MAX_RAND: u128 = (1 << 80) - 1;
 const BASE = "0123456789ABCDEFGHJKMNPQRSTVWXYZ";
 const ENCODED_LEN = 26;
 
-const UlidError = error {
+pub const UlidError = error {
     MaxRand,
 };
 
-const Ulid = struct {
+pub const Ulid = struct {
     time: u64,
     rand: u128,
 
@@ -60,7 +60,7 @@ const Ulid = struct {
     }
 };
 
-fn to_binary(src: u128) [ULID_BIT_LEN:0]u8 {
+pub fn to_binary(src: u128) [ULID_BIT_LEN:0]u8 {
     var v = src;
     var result: [ULID_BIT_LEN:0]u8 = undefined;
     var i: u8 = 0;
@@ -73,7 +73,7 @@ fn to_binary(src: u128) [ULID_BIT_LEN:0]u8 {
    return result;
 }
 
-fn encode_base32(src: u128) [ENCODED_LEN:0]u8 {
+pub fn encode_base32(src: u128) [ENCODED_LEN:0]u8 {
     var buffer: [ENCODED_LEN:0]u8 = undefined;
     var source = src;
     var i: u5 = 0;
@@ -90,12 +90,17 @@ test {
     var generator = Ulid.new();
     const ulid1 = try generator.source();
     const ulid2 = try generator.source();
+    // example: 2010743602507803096168807030878758460
     std.debug.print("{}\n", .{ulid1});
     const bin = to_binary(ulid1);
     const encoded1 = encode_base32(ulid1);
     const encoded2 = encode_base32(ulid2);
 
+    // example: 00000001100000110100000101010000010010111000100101111100011101001110110010011001000010010101110011100111011111011110011000111100
     std.debug.print("{s}\n", .{bin});
+    // Should generate monotonic ulids.
+    // example: 01GD0N0JW9FHTES689BKKQVSHW
+    //          01GD0N0JW9FHTES689BKKQVSHX
     std.debug.print("{s}\n", .{encoded1});
     std.debug.print("{s}\n", .{encoded2});
 }
